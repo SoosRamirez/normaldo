bad89f350e236292beb52981c18ba76bffe7aa37
soos <mrsoosramirez@gmail.com>
2023-11-06 03:34:10 +0300

    smth

--- skins/skinsRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express")
-const controller = require("./skinsController")
-const router = new Router();
-
-router.get('/oneById', controller.getOneById)
-router.get('/getAll', controller.getAll)
-module.exports = router
